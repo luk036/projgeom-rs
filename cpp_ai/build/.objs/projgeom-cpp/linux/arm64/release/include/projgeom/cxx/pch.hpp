37756bd1e4d49d469db6ae0928d0fd04c3426d43
Wai-Shing Luk <luk036@gmail.com>
2025-12-21 13:42:32 +0800

    remove cpp_ai/build

--- cpp_ai/build/.objs/projgeom-cpp/linux/arm64/release/include/projgeom/cxx/pch.hpp
+++
@@ -1,5 +0,0 @@
-#pragma system_header
-#ifdef __cplusplus
-#include "/data/data/com.termux/files/home/github/rs/projgeom-rs/cpp_ai/include/projgeom/pch.hpp"
-#endif // __cplusplus
-                
--- cpp_ai/build/CMakeFiles/projgeom-cpp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for projgeom-cpp.
-# This may be replaced when dependencies are built.
